ae82bb4b3fd8130dc37d8cefce8065f625298ce4
Nilesh <nileshvidhate2020@gmail.com>
2026-05-09 11:25:21 +0530

    Added JWT authentication and user-based authorization

--- backend/middleware/AuthMiddleware.js
+++ backend/middleware/AuthMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
 
     try {
 
@@ -19,6 +20,15 @@ const authMiddleware = (req, res, next) => {
             token,
             process.env.JWT_SECRET
         );
+
+         const user = await User.findById(decoded.userId);
+
+        if (!user) {
+
+            return res.status(401).json({
+                message: "User not found"
+            });
+        }
 
         req.user = decoded;
 
